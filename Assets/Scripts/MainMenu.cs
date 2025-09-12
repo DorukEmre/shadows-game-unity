88744a6f9a90e2c3590dd4fa11bddaa02ccf6852
Doruk Emre <doruk.emre@outlook.com>
2025-09-12 17:05:38 +0200

    Added functionality to MainMenu buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+  public void PlayGame(string mode)
+  {
+
+    Debug.Log("Game mode selected: " + mode);
+
+    PlayerPrefs.SetString("GameMode", mode);
+    UnityEngine.SceneManagement.SceneManager.LoadScene("LevelPicker");
+  }
+
+  public void QuitGame()
+  {
+    Debug.Log("Quitting game...");
+    Application.Quit();
+  }
 }
