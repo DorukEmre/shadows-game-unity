5cf47652f8b263f091492f0e0acc2ff186b169a0
Doruk Emre <doruk.emre@outlook.com>
2025-09-12 21:06:47 +0200

    LevelPicker level box has a button

--- Assets/Scripts/LevelPicker.cs
+++ Assets/Scripts/LevelPicker.cs
@@ -5,12 +5,13 @@ public class LevelPicker : MonoBehaviour
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
-    Debug.Log("Levels completed status:");
+    string completedLevels = "Completed Levels: ";
     for (int i = 0; i < GameManager.Instance.levelsCompleted.Length; i++)
     {
-      Debug.Log("Level " + (i + 1) + ": " + (GameManager.Instance.levelsCompleted[i] ? "Completed" : "Not Completed"));
-
+      completedLevels += (i + 1) + ": " + (GameManager.Instance.levelsCompleted[i] ? "Yes" : "No") + " | ";
     }
+    Debug.Log(completedLevels);
+
   }
 
   // Update is called once per frame
