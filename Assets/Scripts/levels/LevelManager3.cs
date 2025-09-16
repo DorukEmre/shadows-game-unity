1a109983de72dfea022bf14b6676c6d6d8831009
Doruk Emre <doruk.emre@outlook.com>
2025-09-16 17:20:17 +0200

    Added Level 3

---
+++ Assets/Scripts/levels/LevelManager3.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class LevelManager3 : AbstractLevelManager
+{
+  private GameObject levelObject1;
+  private GameObject levelObject2;
+
+  protected override void Start()
+  {
+    base.Start();
+
+    if (levelObjects.Length == 2)
+    {
+      levelObject1 = levelObjects[0];
+      levelObject2 = levelObjects[1];
+    }
+    else
+      Debug.LogError("Level objects error in LevelManager.");
+  }
+
+  protected override void IsWinConditionMet()
+  {
+    float x = levelObject1.transform.rotation.eulerAngles.x;
+    float y = levelObject1.transform.rotation.eulerAngles.y;
+    float z = levelObject1.transform.rotation.eulerAngles.z;
+
+    // Win conditions:
+    // 4: position: (0.10, 1.05, 1.25), rotation: (0.00, 0.00, 0.00)
+    // 2: position: (-0.10, 1.09, 0.96), rotation: (0.00, 0.00, 0.00)
+    // (x), (90 || 270), (90 || 270) 
+    // 270, (45 || 225), (135 || 315)
+    // 270, (135 || 315), (45 || 225)
+    // (90 || 270), (0 || 180), (0 || 180)
+
+    // if (
+    //     ((Is(y, 90) || Is(y, 270)) && (Is(z, 90) || Is(z, 270)))
+    //     || (Is(x, 270) && (Is(y, 45) || Is(y, 225)) && (Is(z, 135) || Is(z, 315)))
+    //     || (Is(x, 270) && (Is(y, 135) || Is(y, 315)) && (Is(z, 45) || Is(z, 225)))
+    //     || ((Is(x, 90) || Is(x, 270)) && (Is(y, 0) || Is(y, 180)) && (Is(z, 0) || Is(z, 180)))
+    //   )
+    // {
+    // TriggerWin();
+    // }
+  }
+}
